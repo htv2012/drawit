337fe59f0dadd72cb81d328874e4788b2bc5c90d
Hai Vu <haivu2004@gmail.com>
2026-06-28 21:43:29 -0700

    chore: use drawtree for text output

--- src/drawit/__init__.py
+++ src/drawit/__init__.py
@@ -1,2 +1,11 @@
-def main() -> None:
-    print("Hello from drawit!")
+import drawtree
+import click
+
+
+@click.command
+@click.argument("seq", nargs=-1)
+def main(seq):
+    seq = ["#" if token=="null" else token for token in seq]
+    text = f"{{{','.join(seq)}}}"
+    drawtree.draw_level_order(text)
+
